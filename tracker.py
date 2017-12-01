219895d04302229c8a673ded5838d8b8605d6e8c
Harvey Chen <harveyc95@gmail.com>
2017-12-01 15:42:24 -0500

    added json parsing

--- tracker.py
+++ tracker.py
@@ -1,18 +1,15 @@
 import urllib2
-import re
-
-response = urllib2.urlopen('https://api.cryptonator.com/api/ticker/eth-cad')
-html = response.read()
+import json
 
 def getPrice(coin, currency):
-	link = 'https://api.cryptonator.com/api/ticker/'
-	link = "-".join([link+coin, currency])
-	response = urllib2.urlopen(link)
-	html = response.read()
-	# print re.findall(r"[\w']+", html)
-	result = html.replace('{',' ').replace('}',' ').replace('\"',' ').replace('\'',' ').replace(':',' ').replace(',',' ').split()
-	print coin + ': ' + currency + ' $' + result[6]
-	return result[6]
+    link = 'https://api.cryptonator.com/api/ticker/'
+    link = link + "-".join([coin, currency])
+    response = urllib2.urlopen(link)
+    html = response.read()
+    data = json.loads(html)
+    return data["ticker"]["price"]
 
 price = getPrice('eth', 'cad')
+print price + ' ' + 'cad'
 price = getPrice('btc', 'usd')
+print price + ' ' + 'usd'
