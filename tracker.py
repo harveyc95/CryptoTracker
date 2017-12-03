f10d12d8ed3d18946c5e711d19134974645c56e4
Harvey Chen <harveyc95@gmail.com>
2017-12-03 18:35:04 -0500

    added class Currency and Transaction class

--- tracker.py
+++ tracker.py
@@ -1,15 +1,27 @@
 import urllib2
 import json
 
-def getPrice(coin, currency):
+class Currency ():
+    def __init__(self, id):
+        self.id = id
+        self.history = []
+
+class Transaction ():
+    def __init__(self, timestamp, rate, amount, fee):
+        self.timestamp = timestamp
+        self.rate = rate
+        self.amount = amount
+        self.fee = fee
+        
+def getPrice(coin_id, currency_id):
     link = 'https://api.cryptonator.com/api/ticker/'
-    link = link + "-".join([coin, currency])
+    link = link + "-".join([coin_id, currency_id])
     response = urllib2.urlopen(link)
     html = response.read()
     data = json.loads(html)
     return data["ticker"]["price"]
 
-price = getPrice('eth', 'cad')
-print price + ' ' + 'cad'
-price = getPrice('btc', 'usd')
-print price + ' ' + 'usd'
+if __name__ == "__main__":
+    CAD = Currency('CAD')
+    BTC = Currency('ETH')
+    BTC = Currency('BTC')
